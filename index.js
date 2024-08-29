a58b6e6a19a1d3faefbcd38d5031d7a2816e882a
Sijan Pokharel <sijanpok55@gmail.com>
2024-08-29 21:11:02 +0545

    middle ware

--- index.js
+++ index.js
@@ -5,7 +5,45 @@ const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const express = require("express");
 const server = express();
 
-server.get("/", (req, res) => {
+server.use((req, res, next) => {
+  console.log(
+    req.get("User-Agent"),
+    req.method,
+    req.ip,
+    new Date(),
+    req.hostname,
+  ); //logger
+  next();
+});
+
+const auth = (req, res, next) => {
+  console.log(req.query);
+  if (req.query.password == 123) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+//API - Endpoint - Route
+server.get("/", auth, (req, res) => {
+  res.json({ type: "GET" });
+});
+server.post("/", (req, res) => {
+  // res.json({ type: "POST" });
+  res.status(201).json({ type: "POST" });
+});
+server.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
+server.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+server.patch("/", (req, res) => {
+  res.json({ type: "PATCH " });
+});
+
+server.get("/prod", (req, res) => {
   //res.send("<h1>Sijan Pokharel </h1>");
   //res.sendFile("/Users/sijanpokharel/Documents/testnode/index.html");
   //res.json(data);

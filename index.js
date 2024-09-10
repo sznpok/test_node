8fb6e4bd42644ad538b929e44b948b02af90f774
Sijan Pokharel <sijanpok55@gmail.com>
2024-09-10 20:56:09 +0545

    refactored

--- index.js
+++ index.js
@@ -18,17 +18,6 @@ server.use(morgan("default "));
 //send static file send through folder
 server.use(express.static("public"));
 
-// server.use((req, res, next) => {
-//   console.log(
-//     req.get("User-Agent"),
-//     req.method,
-//     req.ip,
-//     new Date(),
-//     req.hostname,
-//   ); //logger
-//   next();
-// });
-
 const auth = (req, res, next) => {
   console.log(req.query);
   // if (req.query.password == 123) {
@@ -39,54 +28,74 @@ const auth = (req, res, next) => {
   next();
 };
 
+//refactor code
+
+//create Product Post
+
+const createProduct = (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body);
+};
+
+//GetAll product Get
+const getAllProduct = (req, res) => {
+  res.json(products);
+};
+
+//Get Product by ID
+const getProductByID = (req, res) => {
+  const id = +req.params.id;
+  const product = products.find((p) => p.id === id);
+  res.json(product);
+};
+
+//Update Put
+const putProductByID = (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  products.splice(productIndex, 1, { ...req.body, id: id });
+  res.status(201).json();
+};
+
+//update Patch
+const patchProductByID = (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1, { ...product, ...req.body });
+  res.status(201).json();
+};
+
+//deleete
+
+const deleteProductByID = (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1);
+  res.status(201).json(product);
+};
+
 //API - Endpoint - Route
 server.get("/", auth, (req, res) => {
   res.json({ type: "GET" });
 });
 
 //create products post
-server.post("/products", (req, res) => {
-  console.log(req.body);
-  products.push(req.body);
-  res.status(201).json(req.body);
-});
-
+server.post("/products", createProduct);
 //Get products Read
-server.get("/products", (req, res) => {
-  res.json(products);
-});
-
-server.get("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const product = products.find((p) => p.id === id);
-  res.json(product);
-});
-
+server.get("/products", getAllProduct);
+//get product by id
+server.get("/products/:id", getProductByID);
 //Update put /proudcts/:id
-server.put("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  products.splice(productIndex, 1, { ...req.body, id: id });
-  res.status(201).json();
-});
+server.put("/products/:id", putProductByID);
 
 //Update Patch /products/:id
-server.patch("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1, { ...product, ...req.body });
-  res.status(201).json();
-});
+server.patch("/products/:id", patchProductByID);
 
 //Delete  Delete /products/:id
-server.delete("/products/:id", (req, res) => {
-  const id = +req.params.id;
-  const productIndex = products.findIndex((p) => p.id === id);
-  const product = products[productIndex];
-  products.splice(productIndex, 1);
-  res.status(201).json(product);
-});
+server.delete("/products/:id", deleteProductByID);
 
 //this one at the end always
 server.listen(8080, () => {
